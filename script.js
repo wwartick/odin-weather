1be3d9556e399bc047c9330ad58fda509bfd7bbe
wwartick <willwartick@gmail.com>
2024-03-22 22:01:15 -0700

    some API fetch logic

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('poop')
+const submitButton = document.querySelector('#submit-button')
+
+const submitForm = function() {
+    const userInput = document.querySelector('#location-input');
+    let userLocation = userInput.value;
+    searchWeather(userLocation);
+
+}
+
+const searchWeather = function(location) {
+
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=868a48d93ab64879b79235103242103&q=${location}&days=6&aqi=no&alerts=no`, {mode: 'cors'})
+        .then(function(res){
+            return res.json();
+        }).then(function(res){
+            console.log(res);
+            console.log((res.forecast.forecastday[4].date).substring(5));
+            const forecastDate = new Date(res.forecast.forecastday[4].date);
+            console.log(forecastDate)
+            const foreTest = forecastDate.getDay();
+            const dayNames = ['Sunday', 'Mondayy', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            console.log(dayNames[foreTest]);
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+}
+
+submitButton.addEventListener('click', submitForm);
+

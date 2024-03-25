ed6d3821fa122c1c1cc2f0f92d015ce5025aeed1
wwartick <willwartick@gmail.com>
2024-03-24 21:38:07 -0700

    variable declaration

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const submitForm = function() {
     const userInput = document.querySelector('#location-input');
     let userLocation = userInput.value;
     searchWeather(userLocation);
+}
+
+const generateForecastCards = function(day) {
+    //get date and name of day
+    const dayDate = day.date;
+    const utcDay = new Date(dayDate).getUTCDay();
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const dayName = dayNames[utcDay];
+    const shortDate = dayDate.slice(5);
+
+    //get temperature low, high, and average
+    const averageTempF = Math.floor(day.day.avgtemp_f);
+    const minTempF = Math.floor(day.day.mintemp_f);
+    const maxTempF = Math.floor(day.day.maxtemp_f);
+
+    const minTempC = Math.floor(day.day.mintemp_c);
+    const averageTempC = Math.floor(day.day.avgtemp_c)
+    const maxTempC = Math.floor(day.day.maxtemp_C);
+
+    const dayImage = day.day.condition.icon;
+
+    
 
 }
 
@@ -13,13 +35,13 @@ const searchWeather = function(location) {
         .then(function(res){
             return res.json();
         }).then(function(res){
-            console.log(res);
-            console.log((res.forecast.forecastday[4].date).substring(5));
-            const forecastDate = new Date(res.forecast.forecastday[4].date);
+            const currentDay = res.forecast.forecastday.shift();
+            const forecastDays = res.forecast.forecastday;
+            forecastDays.forEach(day => generateForecastCards(day));
+            /* const forecastDate = new Date(res.forecast.forecastday[4].date);
             const foreTest = forecastDate.getDay();
             const dayNames = ['Sunday', 'Mondayy', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-            const shortNames = dayName.split(0,3)''
-            console.log(dayNames[foreTest]);
+            console.log(dayNames[foreTest]); */
         })
         .catch(function(err){
             console.log(err);
